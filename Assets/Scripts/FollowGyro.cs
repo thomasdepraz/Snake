b610c249c7a133b2c81fc80e8254fb96ded2f2bd
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-01-24 16:57:12 +0100

    First Commit

---
+++ Assets/Scripts/FollowGyro.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FollowGyro : MonoBehaviour
+{
+    [Header("Tweaks")]
+    [SerializeField] private Quaternion baseRotation = new Quaternion(0, 0, 1, 0);
+
+    [Header("Debug")]
+    public GameObject cubeRotation;
+    private Text cubeRotationText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GyroManager.Instance.EnableGyro();
+        cubeRotationText = cubeRotation.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cubeRotationText.text = "Cube Rotation : " + transform.localRotation;
+        Debug.Log("CubeRotation : " + transform.localRotation);
+        transform.localRotation = GyroManager.Instance.GetGyroRotation() * baseRotation;
+    }
+
+    public void ResetRotation()
+    {
+        transform.localRotation = new Quaternion(0, 0, 0, 0);
+        baseRotation = GyroManager.Instance.GetGyroRotation();
+    }
+}
---
+++ Assets/Scripts/Manager/GyroManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GyroManager : MonoBehaviour
+{
+    #region Instance
+    private static GyroManager instance;
+    public static GyroManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GyroManager>();
+                if (instance == null)
+                {
+                    instance = new GameObject("Spawned GyroManager", typeof(GyroManager)).GetComponent<GyroManager>();
+                }
+            }
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+    #endregion
+
+    [Header("Logic")]
+    private Gyroscope gyro;
+    private Quaternion rotation;
+    private bool gyroActive;
+
+    [Header("Debug")]
+    public GameObject phoneRotation;
+    private Text phoneRotationText;
+
+    void Start()
+    {
+        phoneRotationText = phoneRotation.GetComponent<Text>();  
+    }
+    void Update()
+    {
+        if(gyroActive)
+        {
+            rotation = gyro.attitude;
+        }
+    }
+    public void EnableGyro()
+    {
+        if(gyroActive)
+        {
+            return;
+        }
+
+        if(SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            gyroActive = gyro.enabled;
+        }
+    }
+    public Quaternion GetGyroRotation()
+    {
+        phoneRotationText.text = "Phone Rotation : " + rotation;
+        Debug.Log("GyroRotation : " + rotation);
+        return rotation;
+    }
+
+
+}
